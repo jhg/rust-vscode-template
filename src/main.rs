28ad651b80d70e1195faa59cee35ff021ab74825
Jesús Hernández <jesushdez@protonmail.com>
2020-08-09 11:35:14 +0200

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+#![warn(clippy::pedantic)]
+
+//! # {{project-name}}
+//! Executable description.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
