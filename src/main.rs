5ee83f387efc8d021f5c9bc56a2e2b37839b9d10
Jesús Hernández <jesushdez@protonmail.com>
2020-12-08 01:47:03 +0100

    Update main.rs with main function and more lints

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
 #![warn(clippy::pedantic)]
+#![deny(clippy::complexity)]
+#![deny(clippy::cognitive_complexity)]
 
 //! # {{project-name}}
 //! Executable description.
+
+fn main() {
+    // Statements here are executed when the compiled binary is called
+    println!("Hello World!");
+}
 
 #[cfg(test)]
 mod tests {
